053c0c23eedf73f63c8cb02c12ed93a9ef80ca84
Andrew Stoycos <astoycos@redhat.com>
2022-09-19 09:08:21 -0400

    Fix Example
    Fix rust ebpf code
    Add bindings + Parser so we can
    access the ip headers
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- tcbpfd-ebpf/src/main.rs
+++ tcbpfd-ebpf/src/main.rs
@@ -1,24 +1,57 @@
 #![no_std]
 #![no_main]
 
+use core::mem;
 use aya_bpf::{
     macros::classifier,
-    programs::SkBuffContext,
+    programs::TcContext,
+    bindings::TC_ACT_OK,
 };
 use aya_log_ebpf::info;
+use memoffset::offset_of;
+
+mod bindings;
+use bindings::{ethhdr, iphdr};
+
 
 #[classifier(name="tcbpfd")]
-pub fn tcbpfd(ctx: SkBuffContext) -> i32 {
-    match unsafe { try_tcbpfd(ctx) } {
+pub fn tcbpfd(ctx: TcContext) -> i32 {
+    match try_tcbpfd(ctx) {
         Ok(ret) => ret,
         Err(ret) => ret,
     }
 }
 
-unsafe fn try_tcbpfd(ctx: SkBuffContext) -> Result<i32, i32> {
-    info!(&ctx, "received a packet");
-    Ok(0)
+fn try_tcbpfd(ctx: TcContext) -> Result<i32, i32> {    
+    let h_proto = u16::from_be(
+        ctx.load(offset_of!(ethhdr, h_proto))
+            .map_err(|_| TC_ACT_OK)?,
+    );
+
+    match h_proto {
+        ETH_P_IP => {
+            let source = u32::from_be(
+                ctx.load(ETH_HDR_LEN + offset_of!(iphdr, saddr))
+                    .map_err(|_| TC_ACT_OK)?,
+            );
+            let dest = u32::from_be(
+                ctx.load(ETH_HDR_LEN + offset_of!(iphdr, daddr))
+                    .map_err(|_| TC_ACT_OK)?,
+            );
+            info!(&ctx, "source IPv4: {:ipv4}, {:x}, {:X}\ndest IPv4: {:ipv4}, {:x}, {:X}", 
+            source, source, source, dest, dest, dest);
+        }
+        _ => return Ok(TC_ACT_OK),
+    }
+
+
+    Ok(TC_ACT_OK)
 }
+
+const ETH_P_IP: u16 = 0x0800;
+const ETH_P_IPV6: u16 = 0x86DD;
+const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
+
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
--- tcbpfd/src/main.rs
+++ tcbpfd/src/main.rs
@@ -1,8 +1,8 @@
 use aya::programs::{tc, SchedClassifier, TcAttachType};
-use aya::Bpf;
-//use aya_log::BpfLogger;
+use aya::{include_bytes_aligned, BpfLoader};
+use aya_log::BpfLogger;
 use clap::Parser;
-//use log::info;
+use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
 use tokio::signal;
 
 #[derive(Debug, Parser)]
@@ -15,14 +15,40 @@ struct Opt {
 async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
 
-    env_logger::init();
+    TermLogger::init(
+        LevelFilter::Debug,
+        ConfigBuilder::new()
+            .set_target_level(LevelFilter::Error)
+            .set_location_level(LevelFilter::Error)
+            .build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = BpfLoader::new()
+        // finally load the code
+        .load(include_bytes_aligned!(
+            "../../target/bpfel-unknown-none/debug/tcbpfd"
+        ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = BpfLoader::new()
+        // finally load the code
+        .load(include_bytes_aligned!(
+            "../../target/bpfel-unknown-none/release/tcbpfd"
+        ))?;
+
+    BpfLogger::init(&mut bpf)?;
 
     // error adding clsact to the interface if it is already added is harmless
     // the full cleanup can be done with 'sudo tc qdisc del dev eth0 clsact'.
     let _ = tc::qdisc_add_clsact(&opt.iface);
 
-    let mut bpf = Bpf::load_file("/home/afredette/bpf/accept-all.o")?;
-    let program: &mut SchedClassifier = bpf.program_mut("accept").unwrap().try_into()?;
+    let program: &mut SchedClassifier = bpf.program_mut("tcbpfd").unwrap().try_into()?;
     program.load()?;
     program.attach(&opt.iface, TcAttachType::Ingress, 50)?;
 
